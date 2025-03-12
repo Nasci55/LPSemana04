b525c04ec6bf9004470d2db3477136c7b728db45
Goncalo Nascimento <goncalonascimento.gamedesign@gmail.com>
2025-03-12 20:23:51 +0000

    Added Xml

--- BetterDecorador/Program.cs
+++ BetterDecorador/Program.cs
@@ -4,6 +4,12 @@ namespace BetterDecorador
 {
     public class Program
     {
+        /// <summary>
+        /// The main method takes 3 arguments and keeps the first one in string
+        /// converts the second into char and the third in int
+        /// then it prints what comes out of the Decor method
+        /// </summary>
+        /// <param name="args"></param>
         private static void Main(string[] args)
         {
 
@@ -15,6 +21,16 @@ namespace BetterDecorador
             System.Console.WriteLine(Decor(args[0], sym, num));
         }
 
+
+        /// <summary>
+        /// This method takes the arguments in main and converts the char to str
+        /// then it adds the char the number of times equal to the int.
+        /// then returns the chars multiplied before and after the string
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="dec"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
         private static string Decor(string s, char dec, int n)
         {
 
