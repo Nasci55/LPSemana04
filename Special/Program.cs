cc7fb1bcd136f0b4d4da4b87db34df7b0811b85f
Goncalo Nascimento <goncalonascimento.gamedesign@gmail.com>
2025-03-12 22:11:00 +0000

    created method GetSpecial

--- Special/Program.cs
+++ Special/Program.cs
@@ -6,7 +6,24 @@ namespace Special
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Console.Write("Insert n-th number of the special sequence:");
+            string input = Console.ReadLine();
+
+            int num = int.Parse(input);
+
+            System.Console.WriteLine(GetSpecial(num));
+        }
+        private static int GetSpecial(int n)
+        {
+            int firstnum = 0;
+            int secNum = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+
+            }
+
+            return secNum;
         }
     }
 }
